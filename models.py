7538a67fb04ffb423de91bc51fc05a6d8623f75f
Василь Картичак <w.i.k.mailua@gmail.com>
2023-08-17 19:55:15 +0300

    models 2

--- models.py
+++ models.py
@@ -1,11 +1,14 @@
-from
+from sqlalchemy import (
+    Column, Integer, Float, String, DateTime, Boolean,
+)
+from sqlalchemy.dialects.postgresql import JSONB
 
 from database import Base
 
 class User(Base):
     __tablename__ = 'user'
 
-    id = 1
-    name =
-    login =
-    password =
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    login = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
