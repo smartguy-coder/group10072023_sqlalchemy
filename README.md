291a3ff218f5d341a1407e2912a18b775c6e86d9
Василь Картичак <w.i.k.mailua@gmail.com>
2023-08-17 19:58:34 +0300

    alembic

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ https://dbeaver.io/download/
 
 pip install python-dotenv
 pip install sqlalchemy alembic asyncpg psycopg2 psycopg2-binary
+
+
+alembic init migration
+
--- models.py
+++ models.py
@@ -5,6 +5,7 @@ from sqlalchemy.dialects.postgresql import JSONB
 
 from database import Base
 
+
 class User(Base):
     __tablename__ = 'user'
 
@@ -12,3 +13,4 @@ class User(Base):
     name = Column(String, nullable=False)
     login = Column(String, unique=True, index=True, nullable=False)
     password = Column(String, nullable=False)
+    is_conflict = Column(Boolean, default=False)
