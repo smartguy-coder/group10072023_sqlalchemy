7e77eb1067cc3516597b6cf74f77c1570c699c57
Василь Картичак <w.i.k.mailua@gmail.com>
2023-08-17 19:18:13 +0300

    database.py

---
+++ database.py
@@ -0,0 +1,6 @@
+# dialect+driver://username:password@host:port/database
+
+from settings import Settings
+
+DATABASE_URL = f'postgresql+asyncpg://{Settings.DATABASE_USER}:{Settings.DATABASE_PASSWORD}@' \
+               f'{Settings.DATABASE_HOST}:{Settings.DATABASE_PORT}/{Settings.DATABASE_NAME}'
---
+++ settings.py
@@ -0,0 +1,16 @@
+import os
+from dataclasses import dataclass
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+@dataclass
+class Settings:
+    DATABASE_NAME = os.getenv('DATABASE', '')
+    DATABASE_USER = os.getenv('DATABASE_USER', '')
+    DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', '')
+    DATABASE_HOST = os.getenv('DATABASE_HOST', '')
+    DATABASE_PORT = os.getenv('DATABASE_PORT', '')
