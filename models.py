56e8327560dc57e329d0a5a9ffd684e107c0a242
Василь Картичак <w.i.k.mailua@gmail.com>
2023-08-21 18:44:51 +0300

    alchemy new table

--- models.py
+++ models.py
@@ -1,16 +1,38 @@
+import datetime
+
 from sqlalchemy import (
     Column, Integer, Float, String, DateTime, Boolean,
+    ForeignKey,
 )
 from sqlalchemy.dialects.postgresql import JSONB
 
 from database import Base
 
 
-class User(Base):
+class BaseInfoMixin:
+    id = Column(Integer, primary_key=True)
+    notes = Column(String(200))
+    created_at = Column(DateTime, default=datetime.datetime.utcnow, nullable=False)
+
+
+class User(BaseInfoMixin, Base):
     __tablename__ = 'user'
 
-    id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     login = Column(String, unique=True, index=True, nullable=False)
     password = Column(String, nullable=False)
     is_conflict = Column(Boolean, default=False)
+
+    def __repr__(self) -> str:
+        return f'User {self.name} {self.id}'
+
+
+class Order(BaseInfoMixin, Base):
+    __tablename__ = 'order'
+
+    pizza_quantity = Column(Integer, nullable=False)
+    pizza_price = Column(Float, nullable=False)
+    customer = Column(ForeignKey('user.id'))
+
+    def __repr__(self) -> str:
+        return f'Order #{self.id}'
