2ba15d098f4fb619ece202a0f416bf3da85e45e3
Василь Картичак <w.i.k.mailua@gmail.com>
2023-08-21 19:13:17 +0300

    orm2

--- dao.py
+++ dao.py
@@ -2,6 +2,22 @@ import asyncio
 
 from sqlalchemy import insert, select, update, delete
 
+from database import async_session_maker
 from models import User, Order
 
 
+async def create_user(
+        name: str,
+        login: str,
+        password: str,
+        notes: str = None,
+        is_conflict: bool = False,
+):
+    async with async_session_maker() as session:
+        query = insert(User).values(
+            name=name,
+            login=login,
+            password=password,
+            notes=notes,
+            is_conflict=is_conflict,
+        )
