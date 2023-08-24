9c491a21fea17e30693c273c108c9324be3adee0
Василь Картичак <w.i.k.mailua@gmail.com>
2023-08-24 19:56:40 +0300

    api9

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -1,2 +1,12 @@
+import http
 
 
+async def test_main_page_get(client):
+    responce = client.get('/')
+    assert responce.status_code == http.HTTPStatus.OK
+    assert responce.json() == {'greeting': 'HELLO'}
+
+async def test_main_page_post(client):
+    responce = client.get('/')
+    assert responce.status_code == http.HTTPStatus.OK
+    assert responce.json() == {'greeting': 'HELLO'}
