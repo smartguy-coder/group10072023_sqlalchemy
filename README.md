1a757674af25f6f9879353cdd913fe64c56ad66b
Василь Картичак <w.i.k.mailua@gmail.com>
2023-08-24 19:22:04 +0300

    api4

--- README.md
+++ README.md
@@ -4,15 +4,20 @@ https://dbeaver.io/download/
 
 
 pip install python-dotenv
+
 pip install sqlalchemy alembic asyncpg psycopg2 psycopg2-binary
 
 
 alembic init migration
+
 alembic revision --autogenerate -m 'initial' 
+
  alembic upgrade head
+
  alembic downgrade -1
 
 
 pip install fastapi[all] pytest pytest-asyncio
+
 uvicorn app.main:app --reload --port 8000 
 
--- app/main.py
+++ app/main.py
@@ -8,5 +8,14 @@ app = FastAPI(
 
 
 @app.get('/')
-async def main_page():
+@app.post('/')
+async def main_page() -> dict:
     return {'data': 'something'}
+
+
+@app.get('/{user_name}')
+@app.get('/{user_name}/{user_nik}')
+async def user_page(user_name: str, user_nik: str = '', limit: int = 10, skip: int = 0) -> dict:
+    data = [i for i in range(1000)]
+
+    return {'user_name': user_name, 'user_nik': user_nik, 'data': data}
