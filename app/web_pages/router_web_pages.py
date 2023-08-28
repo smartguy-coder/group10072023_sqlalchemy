17138de8c134b443081fa5d813d4de1a2c19f23b
Василь Картичак <w.i.k.mailua@gmail.com>
2023-08-28 18:51:21 +0300

    web7

--- app/web_pages/router_web_pages.py
+++ app/web_pages/router_web_pages.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
 
 
@@ -8,3 +8,18 @@ router = APIRouter(
 )
 
 templates = Jinja2Templates(directory='app\\templates')
+
+
+@router.get('/')
+async def get_main_page(request: Request):
+    context = {
+        'request': request,
+    }
+
+    return templates.TemplateResponse(
+        'base.html',
+        context=context,
+    )
+
+
+
