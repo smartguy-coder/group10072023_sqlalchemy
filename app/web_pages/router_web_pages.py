633b3fa559a0f6ba4cdf0632f10c78507f1e3fb4
Василь Картичак <w.i.k.mailua@gmail.com>
2023-08-28 19:57:40 +0300

    web12

--- app/web_pages/router_web_pages.py
+++ app/web_pages/router_web_pages.py
@@ -10,16 +10,38 @@ router = APIRouter(
 templates = Jinja2Templates(directory='app\\templates')
 
 
-@router.get('/')
-async def get_main_page(request: Request):
+# @router.get('/')
+# async def get_main_page(request: Request):
+#     context = {
+#         'request': request,
+#     }
+#
+#     return templates.TemplateResponse(
+#         'base.html',
+#         context=context,
+#     )
+
+
+@router.get('/menu')
+async def get_menu(request: Request):
     context = {
         'request': request,
+        'title': 'Наше меню',
     }
 
     return templates.TemplateResponse(
-        'base.html',
+        'menu.html',
         context=context,
     )
 
+@router.get('/about-us')
+async def get_menu(request: Request):
+    context = {
+        'request': request,
+        'title': 'Про нас',
+    }
 
-
+    return templates.TemplateResponse(
+        'about_us.html',
+        context=context,
+    )
