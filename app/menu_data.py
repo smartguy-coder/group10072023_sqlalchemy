c9e27588f71686117d493bb20948728f79cd9363
Василь Картичак <w.i.k.mailua@gmail.com>
2023-08-28 18:27:12 +0300

    web3

---
+++ app/menu_data.py
@@ -0,0 +1,20 @@
+menu = [
+    {
+        'title': 'Карбонара',
+        'image': 'image(1).png',
+        'price': 220,
+    },
+    {
+        'title': 'Пепероні',
+        'image': 'image(2).png',
+        'price': 180,
+    },    {
+        'title': 'Барбекю',
+        'image': 'image(3).png',
+        'price': 220,
+    }, {
+        'title': 'Пепероні з копченим мясом',
+        'image': 'image(4).png',
+        'price': 300,
+    },
+]
