255163ecfcc9bdbdb2918d782e9994045ce15a20
Василь Картичак <w.i.k.mailua@gmail.com>
2023-09-04 20:13:16 +0300

    proxy11

--- app/sockets/router_websocket.py
+++ app/sockets/router_websocket.py
@@ -2,6 +2,29 @@ from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 import json
 
 
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+
+
 router = APIRouter(
     prefix='/ws',
 )
@@ -9,8 +32,18 @@ router = APIRouter(
 
 @router.websocket('/')
 async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        text = await websocket.receive_text()
-        await websocket.send_text(f'Your text: {text}')
-        # await websocket.send_json(json.dumps({'count': 5}))
+    # await websocket.accept()
+    # while True:
+    #     text = await websocket.receive_text()
+    #     await websocket.send_text(f'Your text: {text}')
+    #     # await websocket.send_json(json.dumps({'count': 5}))
+
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            # await manager.send_personal_message(f"You wrote: {data}", websocket)
+            await manager.broadcast(f"НОВЕ ПОВІДОМЛЕННЯ: {data}")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        # await manager.broadcast(f"Client #{client_id} left the chat")
