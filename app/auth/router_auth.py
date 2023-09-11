87b806058318227e8dbd2f34751ff480be8e7b71
Василь Картичак <w.i.k.mailua@gmail.com>
2023-09-11 18:23:35 +0300

    auth8

--- app/auth/router_auth.py
+++ app/auth/router_auth.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, status
 
+from .auth_lib import AuthHandler
 from .schemas import AuthDetails, AuthRegistered
 import dao
 
@@ -13,10 +14,12 @@ router = APIRouter(
 @router.post('/register', response_model=AuthRegistered, status_code=status.HTTP_201_CREATED)
 async def register(auth_details: AuthDetails):
 
+    hashed_password = AuthHandler.get_password_hash(auth_details.password)
+
     user_data = await dao.create_user(
         name=auth_details.name,
         login=auth_details.login,
-        password=auth_details.password,
+        password=hashed_password,
         notes=auth_details.notes,
     )
 
