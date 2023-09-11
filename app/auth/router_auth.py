89cdeaa3d7fdde11bca4199a85dfd35d9ffc4d10
Василь Картичак <w.i.k.mailua@gmail.com>
2023-09-11 19:51:06 +0300

    auth12

--- app/auth/router_auth.py
+++ app/auth/router_auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status, HTTPException
+from fastapi import APIRouter, status, HTTPException, Request, Response
 
 from .auth_lib import AuthHandler
 from .schemas import AuthDetails, AuthRegistered
@@ -12,7 +12,10 @@ router = APIRouter(
 
 
 @router.post('/register', response_model=AuthRegistered, status_code=status.HTTP_201_CREATED)
-async def register(auth_details: AuthDetails):
+async def register(request: Request, response: Response, auth_details: AuthDetails):
+    # print(request.cookies,          88888888888888888)
+    # print(request.__dict__,          88888888888888888)
+
     is_login_already_used = await dao.get_user_by_login(auth_details.login)
     if is_login_already_used:
         raise HTTPException(
@@ -28,5 +31,9 @@ async def register(auth_details: AuthDetails):
         password=hashed_password,
         notes=auth_details.notes,
     )
+
+
+    response.set_cookie(key='my_name', value='Vasyl', max_age=10, httponly=True)
+    response.set_cookie(key='token', value=user_data, max_age=10, httponly=True)
 
     return AuthRegistered(success=True, id=user_data[0], login=user_data[1])
